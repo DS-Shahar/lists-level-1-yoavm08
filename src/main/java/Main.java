23c66980b688f639ffffc12ec117738c01dd9a0a
yoavm08 <yoavmarom707@gmail.com>
2024-12-09 10:17:34 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,6 +30,50 @@ public class Main {
         return count;
     }
 
+	public static Node<Integer> q3()
+	{
+		System.out.println("Enter a number");
+	    int num = reader.nextInt();
+	    if (num == -1)
+		return null;
+	    Node<Integer> a = new Node<>(num);
+	    Node<Integer> b = a;
+	    int i = 0;
+	    while(i!= -1)
+	    {
+		System.out.println("Enter a number");
+		    i = reader.nextInt();
+		    if(i != -1)
+		    {
+			b.setNext(new Node<>(i));
+			b = b.getNext();
+		    }
+	    }
+	    return a;
+	}
+	
+	
+	public static void q4(Node<Integer> a)
+	{
+		Node<Integer> b = a;
+	    while(b != null)
+	    {
+		if (b.getValue() % 2 == 0)
+		    System.out.println(b.getValue());
+		b = b.getNext();
+	    }
+	}
+	
+	
+	public static boolean q5(Node<Integer> a, int b)
+	{
+	    if (a == null)
+		return false;
+	    if (a.getValue() == b)
+		return true;
+	    return q5(a.getNext(), b);
+	}
+
     public static void main(String[] args) 
     {
         Node<Integer> a = q1(1, 10, 10);
